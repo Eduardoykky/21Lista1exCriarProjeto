6729800745a94c977cbd8cc588ae55c84e35979a
Eduardo Oliveira <eduardoykky@gmail.com>
2022-11-14 20:21:54 -0300

    primeiro commit

---
+++ 21Lista1.js
@@ -0,0 +1,9 @@
+
+let nomeProj = []
+let orçamentoTotal = []
+
+function CriarProjeto() {
+    nomeProj.push(prompt("Insira um nome para o projeto"))
+    orçamentoTotal.push(parseInt(prompt("Insira o orçamento total para o projeto.")))
+}
+

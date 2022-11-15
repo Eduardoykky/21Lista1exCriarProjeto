0c7e06cd9856f79cae845e3237eeeab9479d6585
Eduardo Oliveira <eduardoykky@gmail.com>
2022-11-14 21:52:10 -0300

    segundo commit

--- 21Lista1.js
+++ 21Lista1.js
@@ -7,3 +7,17 @@ function CriarProjeto() {
     orçamentoTotal.push(parseInt(prompt("Insira o orçamento total para o projeto.")))
 }
 
+let tipoDespeja
+
+function AtualizarProjeto(nome, valor, tipoD) {
+    let index = nomeProj.indexOf(nome)
+
+    if (tipoDespeja == "r") {
+        orçamentoTotal[index] = orçamentoTotal[index] + valor
+    }else if (valor <= orçamentoTotal[index]) {
+        orçamentoTotal[index] = orçamentoTotal[index] - valor
+    }else if (valor >= orçamentoTotal[index]) {
+        console.log("ERRO")
+    }
+    console.log(orçamentoTotal[index])
+}
